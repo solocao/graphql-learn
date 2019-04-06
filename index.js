1768baf4b9647aa5c42c1ec83acc39751e698ff6
solocao <solocao@126.com>
2019-04-06 15:43:59 +0800

    简单的一波

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ import express from 'express';
 import mongoose, { Schema } from 'mongoose';
 import graphqlHTTP from 'express-graphql';
 
-// import schema from './graphql';
+import schema from './graphql';
 
 const app = express();
+
+mongoose.connect('mongodb://aaaa:graphql_mima@47.99.152.43:27017/graphql', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', () => {
+  console.log('数据库连接失败')
+}).once('open', () => {
+  console.log('数据库连接成功')
+})
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello world, this is graphql api.')
